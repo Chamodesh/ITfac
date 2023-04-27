7b34b446692335614befdb76db115815152ae1e9
Chamod Kalindaka Eshwarage <chamodeshwarage@gmail.com>
2023-04-27 09:18:46 +0530

    Second Commit

---
+++ lib/screens/stu/student_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:itschool/screens/stu/stu_screen/chat_Divider.dart';
+import 'package:itschool/logIn/login_screen.dart';
+import 'package:itschool/screens/stu/components/stHomeBody.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:itschool/screens/stu/sidebar/navigation_drawer_widget.dart';
+import 'package:itschool/screens/stu/stu_screen/CalenderScreen.dart';
+import 'package:itschool/screens/stu/stu_screen/news.dart';
+
+class StuHome extends StatefulWidget {
+  @override
+  State<StuHome> createState() => _StuHomeState();
+}
+
+class _StuHomeState extends State<StuHome> {
+  int currentIndex = 0;
+
+  final screens = [
+    Body(),
+    Calender_Screen(),
+    ChScreen(),
+    News(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 0,
+          leading: Builder(
+              builder: (context) => IconButton(
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                    icon: SvgPicture.asset("assets/icons/menu.svg"),
+                  )),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {},
+            ),
+          ]),
+      drawer: NavigationDrawerWidget(),
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        //backgroundColor: Colors.white54,
+        selectedItemColor: Color(0xff0b3140),
+        unselectedItemColor: Color(0xff384c54),
+        showUnselectedLabels: false,
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calender',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.forum),
+            label: 'Forum',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: 'News',
+          ),
+        ],
+      ),
+    );
+  }
+}
